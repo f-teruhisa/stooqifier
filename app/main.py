4413cd669cff07d31574c35eb3ba8d1f628e2376
f-teruhisa <teru_fukumoto@outlook.jp>
2021-01-01 19:43:46 +0900

    [Add] Get a csv file of the stock price for one year
    Get a csv of price for one year of the stock specified by STOCK_CODE
    in .env with pandas_datareader.After that, delete the csv file.
    
    Because, if the csv file remains after the execution of main.py,
    the past csv files will be accumulated as garbage.

--- app/main.py
+++ app/main.py
@@ -1,2 +1,24 @@
-# Print "Hello World!" to verify that python environment is available
-print("Hello, World!")
+"""
+Get a csv of the stock price for one year of the stock specified by STOCK_CODE in .env with pandas_datareader.
+After that, delete the csv file.
+"""
+import datetime
+import os
+from os.path import join, dirname
+import pandas_datareader.stooq as stooq
+from dotenv import load_dotenv
+from dateutil.relativedelta import relativedelta
+
+today = datetime.date.today()
+start_date = today - relativedelta(years=1)
+end_date = today
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+stock_code = os.environ.get("STOCK_CODE")
+
+stooq_reader = stooq.StooqDailyReader(stock_code, start=start_date, end=end_date)
+data = stooq_reader.read()
+FILENAME = '%s.csv' % str(today)
+data.to_csv(FILENAME)
+os.remove(FILENAME)
