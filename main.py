e0fa16eae620b83585d31fa404982f2410a1d55f
f-teruhisa <teru_fukumoto@outlook.jp>
2021-01-17 13:34:35 +0900

    [Fix] Change tmp directory specification to `/tmp`
    When specifying tmp directory on Cloud Functions,
    it was necessary to specify `/tmp`, which has been fixed.
    
    With the content of this commit,
    `main.py` is now working properly on Cloud Functions.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ csv: Perform operations such as extracting strings from a CSV file downloaded wi
 datetime: Get a date for getting stock prices with stooq API
 os: Get env file and delete csv and image files
 join, dirname: Get env file
-shutil: Remove tmp file(includes csv & chart image files)
 pandas: Read and sort data in a CSV file
 mplfinance: Generate a chart image of stock price
 pandas_datareader.stooq: API client for stooq
@@ -19,7 +18,6 @@ import csv
 import datetime
 import os
 from os.path import join, dirname
-import shutil
 import pandas as pd
 import mplfinance as mpf
 import pandas_datareader.stooq as stooq
@@ -32,12 +30,12 @@ def generate_stock_chart_image():
     """
     Generate a six-month stock chart image with mplfinance
     """
-    dataframe = pd.read_csv(f"tmp/{str(today)}.csv", index_col=0, parse_dates=True)
+    dataframe = pd.read_csv(f"/tmp/{str(today)}.csv", index_col=0, parse_dates=True)
     # The return value `Date` from stooq is sorted by asc, so change it to desc for plot
     dataframe = dataframe.sort_values('Date')
     mpf.plot(dataframe, type='candle', figratio=(12,4),
          volume=True, mav=(5, 25), style='yahoo',
-         savefig=f"tmp/{str(today)}.png")
+         savefig=f"/tmp/{str(today)}.png")
 
 def generate_csv_with_datareader():
     """
@@ -45,7 +43,7 @@ def generate_csv_with_datareader():
     """
     start_date = today - relativedelta(months=3)
     stooq_reader = stooq.StooqDailyReader(stock_code, start=start_date, end=today)
-    stooq_reader.read().to_csv(f"tmp/{str(today)}.csv")
+    stooq_reader.read().to_csv(f"/tmp/{str(today)}.csv")
 
 def main():
     """
@@ -53,7 +51,7 @@ def main():
     """
     generate_csv_with_datareader()
     generate_stock_chart_image()
-    with open(f"tmp/{str(today)}.csv", 'r', encoding="utf-8") as file:
+    with open(f"/tmp/{str(today)}.csv", 'r', encoding="utf-8") as file:
         # Skip header row
         reader = csv.reader(file)
         header = next(reader)
@@ -62,9 +60,6 @@ def main():
             if i == 0:
                 slack.Slack(today, row).post()
 
-    # Remove ./tmp directory. Because if some file remains, stack as garbage
-    shutil.rmtree("tmp")
-
 # Load env variants
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -72,7 +67,11 @@ stock_code = os.environ.get("STOCK_CODE")
 
 # Get today's date for getting the stock price and csv&image filename
 today = datetime.date.today()
-os.mkdir('tmp')
+
+# tmp directory is present by default on Cloud Functions, so guard it
+if not os.path.isdir('/tmp'):
+    os.mkdir('/tmp')
+
 FILENAME = '%s.csv' % str(today)
 
 main()
